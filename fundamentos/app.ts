399c314eb2721024a38ae0b8996b2188b201d205
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-09-30 22:49:27 -0300

    Iniciando

---
+++ fundamentos/app.ts
@@ -0,0 +1,19 @@
+// const soma = (a:number,b:number) => {
+//     console.log(a+b);
+// }
+
+// soma(2,3);
+
+//var a = "A"; // Não recomendado
+//const c = "C";
+
+let b: string = "B";
+let n: number = 2;
+let x : boolean = true;
+
+let m: string | number = 2;
+
+m = "André"
+m = false;
+
+console.log(m);
